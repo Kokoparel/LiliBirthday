82e6584df35d141981a7e9ddd4b169adab0b2845
Kokoparel <157900352+Kokoparel@users.noreply.github.com>
2024-05-02 20:28:26 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function createConfetti() {
     const colors = ['#FFC0CB', '#FFD700', '#8A2BE2', '#00FF00', '#00BFFF', '#FF6347']; // Fun colors
-    const confettiCount = 150;
+    const confettiCount = 400;
     const confettiWrapper = document.getElementById('confetti-wrapper');
 
     for (let i = 0; i < confettiCount; i++) {
@@ -23,16 +23,35 @@ function createConfetti() {
         const rotation = Math.random() * 360;
         const xEnd = Math.random() * (window.innerWidth / 2) - window.innerWidth / 4;
         const yEnd = window.innerHeight + 100;
-        const duration = Math.random() * 2 + 2;
+        const duration = Math.random() * 3.5 + 2;
 
         confetti.style.animation = `confetti-fall ${duration}s ease-in-out forwards`;
-        confetti.style.animationDelay = `${Math.random() * 15}s`; // Shorter delay
+        confetti.style.animationDelay = `${Math.random() * 25}s`; // Shorter delay
         confetti.style.transform = `translateY(${initialYPosition}vh) rotate(${rotation}deg)`;
 
         confettiWrapper.appendChild(confetti);
     }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    createConfetti();
+document.addEventListener('DOMContentLoaded', function() {
+    // Ambil elemen kartu
+    var cardFront = document.querySelector('.cardFront');
+
+    // Tambahkan event listener untuk saat kartu ditekan
+    cardFront.addEventListener('click', function() {
+        // Mulai pemutaran audio saat kartu diklik
+        var audio = document.querySelector('audio');
+        audio.play();
+
+        // Panggil fungsi untuk membuat confetti jatuh
+        createConfetti();
+        
+        // Toggling the opening/closing of the card
+        if (cardFront.style.transform === 'rotateY(-160deg)') {
+            cardFront.style.transform = 'rotateY(0deg)';
+        } else {
+            cardFront.style.transform = 'rotateY(-160deg)';
+        }
+    });
 });
+
